8324cd87f2076ebde945f3ae55c4bcb25ad69073
Ilya <61024440+milia20@users.noreply.github.com>
2023-12-22 01:29:55 +0300

    Delete file_uploader directory

--- file_uploader/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from file_uploader.models import UploadFile, User
-
-admin.site.register(UploadFile)
-admin.site.register(User)
--- file_uploader/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FileUploaderConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'file_uploader'
